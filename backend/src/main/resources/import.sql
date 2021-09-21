c7042eb1f637b99ef160de851466413a0118c799
Alexsandro Machado <lekomachado@hotmail.com>
2021-09-21 09:31:18 -0300

    Database seeding

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,8 @@
+insert into tb_category (name) values ('Books');
+insert into tb_category (name) values ('Eletronics');
+insert into tb_category (name) values ('Cell Phones');
+insert into tb_category (name) values ('Clothing');
+insert into tb_category (name) values ('Home & Kitchen');
+insert into tb_category (name) values ('Tools & Home Improvement');
+insert into tb_category (name) values ('Automotive Parts & Accessories');
+insert into tb_category (name) values ('Beauty & Personal Care');
