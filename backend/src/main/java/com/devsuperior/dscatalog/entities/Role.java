5b1c94a242cb1417728bbc8391e7c8ff530449e5
Alexsandro Machado <lekomachado@hotmail.com>
2022-04-03 10:16:48 -0300

    User CRUD

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Role.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Role.java
@@ -19,16 +19,22 @@ public class Role implements Serializable{
 	private Long id;
 	private String authority;
 	
-	public Role() {
-		
+	public Role() {		
 	}
 	
 	public Role(Long id, String authority) {
-		super();
 		this.id = id;
 		this.authority = authority;
 	}	
 	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 	public String getAuthority() {
 		return authority;
 	}
