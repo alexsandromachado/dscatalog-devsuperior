30470865fa36ca3ebdcfbc6db8385a4e44bc6d9c
Alexsandro Machado <lekomachado@hotmail.com>
2022-06-10 19:12:47 -0300

    test update

--- frontweb/src/components/Navbar/index.tsx
+++ frontweb/src/components/Navbar/index.tsx
@@ -5,6 +5,7 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-md navbar-dark bg-primary main-nav">
       <div className="container-fluid">
+        
         <a href="link" className="nav-logo-text">
           <h4>DS Catalog</h4>
         </a>
