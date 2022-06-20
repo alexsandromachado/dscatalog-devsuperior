6a0e4348c820baae5912a7696f00fe331cea9e47
Alexsandro Machado <lekomachado@hotmail.com>
2022-06-20 17:10:38 -0300

    Props

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,4 +1,5 @@
 import './assets/styles/custom.scss';
+
 import './App.css';
 
 import Routes from 'Routes';
--- frontweb/src/pages/Catalog/index.tsx
+++ frontweb/src/pages/Catalog/index.tsx
@@ -4,19 +4,19 @@ const Catalog = () => {
   return (
     <div className="container my-4">
       <div className="row">
-        <div className="col-sm-6 col-lg-4 col-xl-3">
+        <div className="col-sm-6 col-lg-4">
           <ProductCard />
         </div>
-        <div className="col-sm-6 col-lg-4 col-xl-3">
+        <div className="col-sm-6 col-lg-4">
           <ProductCard />
         </div>
-        <div className="col-sm-6 col-lg-4 col-xl-3">
+        <div className="col-sm-6 col-lg-4">
           <ProductCard />
         </div>
-        <div className="col-sm-6 col-lg-4 col-xl-3">
+        <div className="col-sm-6 col-lg-4">
           <ProductCard />
         </div>
-        <div className="col-sm-6 col-lg-4 col-xl-3">
+        <div className="col-sm-6 col-lg-4">
           <ProductCard />
         </div>
       </div>
