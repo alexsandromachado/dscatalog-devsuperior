ffd8019829aa5418e3f499211c234efc08007e65
Alexsandro Machado <lekomachado@hotmail.com>
2022-06-21 09:46:05 -0300

    ProductDetails route

--- frontweb/src/Routes.tsx
+++ frontweb/src/Routes.tsx
@@ -3,6 +3,7 @@ import Home from 'pages/Home';
 import Navbar from 'components/Navbar';
 import Catalog from 'pages/Catalog';
 import Admin from 'pages/Admin';
+import ProductDetails from 'pages/ProductDetails/intex';
 
 const Routes = () => (
   <BrowserRouter>
@@ -11,11 +12,14 @@ const Routes = () => (
       <Route path="/" exact>
         <Home />
       </Route>
-      <Route path="/products">
+      <Route path="/products" exact>
         <Catalog />
       </Route>
       <Route path="/admin">
         <Admin />
+      </Route>
+      <Route path="/product:productId">
+        <ProductDetails />
       </Route>
     </Switch>
   </BrowserRouter>
--- frontweb/src/components/ProductPrice/index.tsx
+++ frontweb/src/components/ProductPrice/index.tsx
@@ -1,14 +1,15 @@
+import { formatPrice } from 'util/formatters';
 import './styles.css';
 
 type Props = {
   price: number;
-}
+};
 
 const ProductPrice = ( { price } : Props) => {
   return (
     <div className="product-price-container">
       <span>R$</span>
-      <h3>{price}</h3>
+      <h3>{formatPrice(price)}</h3>
     </div>
   );
 };
