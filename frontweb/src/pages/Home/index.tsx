41d0d2fb837890f5e52cfc01d7a6b1ef106f2f6e
Alexsandro Machado <lekomachado@hotmail.com>
2022-06-29 19:04:46 -0300

    hasAnyRoles

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -1,15 +1,17 @@
 import { ReactComponent as MainImage } from 'assets/images/main-image.svg';
 import ButtonIcon from 'components/ButtonIcon';
 import { Link } from 'react-router-dom';
-import { getTokenData, isAuthenticated } from 'util/requests';
+import { getTokenData, hasAnyHoles, isAuthenticated } from 'util/requests';
 
 import './styles.css';
 
 const Home = () => {
   return (
     <div className="home-container">
-      <h1>{isAuthenticated() ? 'Autenticado' : 'Não autenticado'}</h1>
-
+      <h1>
+        Resultado ={' '}
+        {hasAnyHoles(['ROLE_ADMIN']) ? 'sim' : 'não'}
+      </h1>
       <div className="base-card home-card">
         <div className="home-content-container">
           <div>
