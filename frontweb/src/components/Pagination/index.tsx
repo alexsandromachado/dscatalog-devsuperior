439ae73683de0cc20a6f1f297319ee50249894b4
Alexsandro Machado <lekomachado@hotmail.com>
2022-07-11 15:14:30 -0300

    Pagianation layout

--- frontweb/src/components/Pagination/index.tsx
+++ frontweb/src/components/Pagination/index.tsx
@@ -4,24 +4,20 @@ import './styles.css';
 
 const Pagination = () => {
   return (
-    <>
-
-<ReactPaginate 
-    pageCount={10}
-    pageRangeDisplayed={3}
-    marginPagesDisplayed={1}
-/>
-
-    <div className="pagination-container">
-      <ArrowIcon className="arrow-previous arrow-inactive"/>
-      <div className="pagination-itens active">1</div>
-      <div className="pagination-itens">2</div>
-      <div className="pagination-itens">3</div>
-      <div className="pagination-itens">...</div>
-      <div className="pagination-itens">10</div>
-      <ArrowIcon className="arrow-next arrow-active"/>
-    </div>
-    </>
+    <ReactPaginate
+      pageCount={10}
+      pageRangeDisplayed={3}
+      marginPagesDisplayed={1}
+      containerClassName="pagination-container"
+      pageLinkClassName="pagination-itens"
+      breakClassName="pagination-itens"
+      previousClassName="arrow-previous"
+      nextClassName="arrow-next"
+      activeLinkClassName="pagination-link-active"
+      disabledClassName="arrow-inactive"
+      previousLabel={<ArrowIcon />}
+      nextLabel={<ArrowIcon />}
+    />
   );
 };
 
