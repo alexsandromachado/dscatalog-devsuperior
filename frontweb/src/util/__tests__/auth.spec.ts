0c3deb7c4858cf4804ff26310bce0c3d77845cb3
alexsandromachado <lekomachado@hotmail.com>
2022-07-22 08:24:30 -0300

    Testing another hasAnyRoles scenario

--- frontweb/src/util/__tests__/auth.spec.ts
+++ frontweb/src/util/__tests__/auth.spec.ts
@@ -10,13 +10,25 @@ describe('hasAnyHoles tests', () => {
 
     test('should return true when user has given role', () => {
 
-jest.spyOn(TokenModule, 'getTokenData').mockReturnValue({
-    exp: 0,
-    user_name:  '',
-    authorities: ['ROLE_ADMIN', 'ROLE_OPERATOR']
-})
+        jest.spyOn(TokenModule, 'getTokenData').mockReturnValue({
+            exp: 0,
+            user_name: '',
+            authorities: ['ROLE_ADMIN', 'ROLE_OPERATOR']
+        })
 
         const result = hasAnyRoles(['ROLE_ADMIN']);
         expect(result).toEqual(true);
     });
+});
+
+test('should return false when user does not have given role', () => {
+
+    jest.spyOn(TokenModule, 'getTokenData').mockReturnValue({
+        exp: 0,
+        user_name: '',
+        authorities: ['ROLE_OPERATOR']
+    })
+
+    const result = hasAnyRoles(['ROLE_ADMIN']);
+    expect(result).toEqual(false);
 });
